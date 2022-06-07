ad71330c2c59ff440545b6d464dcc43dcd096a49
KateWld <kirabastet@yandex.ru>
2022-06-07 18:22:58 +0300

    Первая домашка

---
+++ src/Lesson1/Lesson.java
@@ -0,0 +1,23 @@
+package Lesson1;
+
+public class Lesson {
+    public static void main(String[] args) {
+
+        String hi = "                Hello ";
+        String world = " WoRld!";
+        char newLine = '\n';
+
+        System.out.println();
+
+
+        double growth = 14;
+        double growthCount = growth * growth;
+        double weight = 34;
+        double indexWeight = weight / growthCount;
+
+        System.out.println(indexWeight);
+
+    }
+
+
+}
--- src/NewTest.java
+++ src/NewTest.java
@@ -1,6 +1,6 @@
-public class NewTest {
+//public class NewTest {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+    //public static void main(String[] args) {
+        //System.out.println("Hello world!");
+  //  }
+//}
