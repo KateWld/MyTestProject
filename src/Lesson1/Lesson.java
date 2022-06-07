07a4d71dd011dd017af6eaf144c5c302616c0ccd
KateWld <kirabastet@yandex.ru>
2022-06-07 21:06:31 +0300

    Первая домашка

--- src/Lesson1/Lesson.java
+++ src/Lesson1/Lesson.java
@@ -1,5 +1,8 @@
 package Lesson1;
 
+import java.util.Arrays;
+import java.util.Locale;
+
 public class Lesson {
     public static void main(String[] args) {
 
@@ -7,17 +10,36 @@ public class Lesson {
         String world = " WoRld!";
         char newLine = '\n';
 
-        System.out.println();
+        String trimmedHi = hi.trim();
+        String worldToLowerCase = world.toLowerCase();
 
+        String result = trimmedHi + worldToLowerCase +  newLine;
 
-        double growth = 14;
+        String repeated = result.repeat(3);
+
+        System.out.println(repeated);
+
+        double growth = 185;
         double growthCount = growth * growth;
-        double weight = 34;
+        double weight = 90;
         double indexWeight = weight / growthCount;
 
         System.out.println(indexWeight);
 
+
+        char[] myArray = new char[5];
+        myArray[0] = 'a';
+        myArray[1] = 'b';
+        myArray[2] = 'c';
+        myArray[3] = 'd';
+        myArray[4] = 'e';
+
+        Arrays.toString(myArray);
+
+        System.out.println(myArray);
+
+        char[] myArray2 = myArray;
+        myArray[4] = 'h';
+        System.out.println(myArray2);
     }
-
-
 }
